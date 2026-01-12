6b185232d64efd00ff0ea73f5aea1de28238e878
Elham Khodabandeh <elham.khodabandeh@dci-student.org>
2026-01-12 11:23:06 +0100

    deposit seite hinzugefügt, auf die man kommt wenn man auf den deposit button drückt

---
+++ src/app/deposit/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function DepositPage() {
+    return (
+        <main className="min-h-screen bg-black px-6 pt-8 pb-24">
+            <h1>Deposit</h1>
+        </main>
+    );
+}
