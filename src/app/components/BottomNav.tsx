035c5f701865a0fb43f1764e675cedbc40dda42d
Elham Khodabandeh <elham.khodabandeh@dci-student.org>
2026-01-12 14:40:52 +0100

    im global.css die farben für die aktiven icons gesetzt

--- src/app/components/BottomNav.tsx
+++ src/app/components/BottomNav.tsx
@@ -7,11 +7,11 @@ export default function BottomNav() {
     return (
         <nav className='fixed left-0 right-0 bottom-0 bg-pink-600 p-4'>
         <div className='flex justify-around'>
-           <span className='nav-item active'><Link href="/dashboard" className='text-white'><House size={28} /></Link></span>
-           <span className='nav-item'><Link href="/portfolio" className='text-white/50'><ChartPie size={28} /></Link></span>
-           <span className='nav-item'><Link href="/search" className='text-white/50'><Search size={28} /></Link></span>
-           <span className='nav-item'><Link href="/profile" className='text-white/50'><User size={28} /></Link></span>
-           <span className='nav-item'><Link href="/menu" className='text-white/50'><Menu size={28} /></Link></span>
+           <span className='nav-item active'><Link href="/dashboard"><House size={28} /></Link></span>
+           <span className='nav-item text-white/50'><Link href="/portfolio"><ChartPie size={28} /></Link></span>
+           <span className='nav-item text-white/50'><Link href="/search"><Search size={28} /></Link></span>
+           <span className='nav-item text-white/50'><Link href="/profile"><User size={28} /></Link></span>
+           <span className='nav-item text-white/50'><Link href="/menu"><Menu size={28} /></Link></span>
         </div>
       </nav>
     );
