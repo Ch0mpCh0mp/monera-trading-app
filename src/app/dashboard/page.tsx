5dcbe16d42bb7c2cec7973d9674936054f902084
Elham Khodabandeh <elham.khodabandeh@dci-student.org>
2026-01-12 14:11:00 +0100

    links zu den icons hinzugefügt und aktiven home icon hervorgehoben

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -30,12 +30,12 @@ export default function DashboardPage() {
       </div>
 
       <nav className='fixed left-0 right-0 bottom-0 bg-pink-600 p-4'>
-        <div className='flex justify-around '>
-           <Link href="/dashboard"><House size={28} /></Link>
-           <span><ChartPie size={28} /></span>
-           <span><Search size={28} /></span>
-           <span><User size={28} /></span>
-           <span><Menu size={28} /></span>
+        <div className='flex justify-around'>
+           <Link href="/dashboard" className='text-white'><House size={28} /></Link>
+           <Link href="/portfolio" className='text-white/50'><ChartPie size={28} /></Link>
+           <Link href="/search" className='text-white/50'><Search size={28} /></Link>
+           <Link href="/profile" className='text-white/50'><User size={28} /></Link>
+           <Link href="/menu" className='text-white/50'><Menu size={28} /></Link>
         </div>
       </nav>
     </main>
