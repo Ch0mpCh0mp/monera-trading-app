d12661e4c64838ded163aa8f9f5984a51442c54c
Elham Khodabandeh <elham.khodabandeh@dci-student.org>
2026-01-18 13:19:40 +0100

    die portfolio watchlist mit den data client laden

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,12 +1,25 @@
-"use client";
-
 import BottomNav from '../components/BottomNav';
 import Logo from '../components/Logo';
+import { getWatchlist } from '@/data/tradingClient';
+import AssetRow from '../components/AssetRow';
 
-export default function PortfolioPage() {
+export default async function PortfolioPage() {
+    const watchlist = await getWatchlist();
+    
     return (
         <main>
             <h1>Portfolio</h1>
+            <section>
+                {watchlist.map((asset) => (
+                    <AssetRow
+                    key={asset.id}
+                    name={asset.name}
+                    symbol={asset.symbol}
+                    price={asset.price}
+                    changePct={asset.changePct}
+                    trend={asset.trend} />
+                ))}
+            </section>
             <BottomNav />
         </main>
     );
